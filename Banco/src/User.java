19c59ec7b4bce02481a93171647c21d2ad71a853
Felipe <90427605+the-felipe@users.noreply.github.com>
2021-12-06 23:36:12 -0300

    Modificação para salvamento em '.bin'

--- Banco/src/User.java
+++ Banco/src/User.java
@@ -1,9 +1,9 @@
-public class User {
+import java.io.Serializable;
 
+public class User implements Serializable {
     private String nome, sobrenome;
     private int ID;
     private float saldo;
-
     // constructors
     public User(String nome, String sobrenome, int ID, float saldo) {
         this.nome = nome;
@@ -11,39 +11,30 @@ public class User {
         this.ID = ID;
         this.saldo = saldo;
     }
-
     // getters
     public String getNome() {
         return nome;
     }
-
     public String getSobrenome() {
         return sobrenome;
     }
-
     public int getID() {
         return ID;
     }
-
     public float getSaldo() {
         return saldo;
     }
-
     // setters
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public void setSobrenome(String sobrenome) {
         this.sobrenome = sobrenome;
     }
-
     public void setID(int ID) {
         this.ID = ID;
     }
-
     public void setSaldo(float saldo) {
         this.saldo = saldo;
     }
-
 }
